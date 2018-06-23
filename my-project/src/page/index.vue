659aa08a2e5b9c0a46987d78797e0274b774c0b6
fengzhiwei <949516000@qq.com>
2018-06-23 14:55:41 +0800

    232

--- my-project/src/page/index.vue
+++ my-project/src/page/index.vue
@@ -1,3 +1,3 @@
 <template>
-    <div>index</div>
+    <div>index page</div>
 </template>
---
+++ my-project/src/router/index.js
@@ -0,0 +1,20 @@
+//1.引用vue
+import Vue from 'vue'
+
+//引用路由
+import Router from 'vue-router'
+
+import Index from '@/page/index'
+
+import Content from '@/page/content'
+//使用路由
+Vue.use(Router);
+export default new Router({
+  routes: [{
+    path: '/',
+    component: Index
+  }, {
+    path: '/content/:id',
+    component: Content
+  }]
+})
