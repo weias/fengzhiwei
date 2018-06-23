e47fef3613de9fa6daaaa54b96e706b3d3ce6b3a
fengzhiwei <949516000@qq.com>
2018-06-23 18:10:20 +0800

    2018.6.23 周六快乐

--- wei788a/src/router/index.js
+++ wei788a/src/router/index.js
@@ -1,6 +1,22 @@
+// import Vue from 'vue'
+// import Router from 'vue-router'
+// import HelloWorld from '@/components/HelloWorld'
+
+// Vue.use(Router)
+
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'HelloWorld',
+//       component: HelloWorld
+//     }
+//   ]
+// })
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Index from '@/page/index'
+import Content from '@/page/content'
 
 Vue.use(Router)
 
@@ -8,8 +24,10 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: Index
+    }, {
+      path: '/content/:id',
+      component: Content
     }
   ]
 })
